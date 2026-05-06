6b2f5971c1bd66e4d4a06e0631d44c8f155e71da
DF89 <14329509+df89@user.noreply.gitee.com>
2026-05-06 15:59:40 +0800

    Remove target/ from tracking, add .gitignore

--- target/aarch64-apple-darwin/debug/deps/cargo_ocd-26f464f509bc8a2b.d
+++
@@ -1,5 +0,0 @@
-/Volumes/CX/学习/Rust_STM32/tools/cargo-ocd/target/aarch64-apple-darwin/debug/deps/cargo_ocd-26f464f509bc8a2b.d: src/main.rs
-
-/Volumes/CX/学习/Rust_STM32/tools/cargo-ocd/target/aarch64-apple-darwin/debug/deps/libcargo_ocd-26f464f509bc8a2b.rmeta: src/main.rs
-
-src/main.rs:
