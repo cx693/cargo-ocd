a524e00dc9c31bce985c36f680ed2781c49487fb
DF89 <14329509+df89@user.noreply.gitee.com>
2026-05-07 11:36:10 +0800

    去除必要的文件

--- target/debug/cargo-ocd.d
+++ target/debug/cargo-ocd.d
@@ -1,1 +1,1 @@
-/Volumes/CX/学习/Rust_STM32/tools/cargo-ocd/target/debug/cargo-ocd: /Volumes/CX/学习/Rust_STM32/tools/cargo-ocd/src/main.rs
+/Volumes/CX/学习/Rust_STM32/tools/cargo-ocd/target/debug/cargo-ocd: /Volumes/CX/学习/Rust_STM32/tools/cargo-ocd/target/package/cargo-ocd-0.1.4/src/main.rs
